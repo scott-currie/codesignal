359d56714a19afe589521056da1be8208ba9718c
Scott Currie <scurrie@ad.nsd.org>
2018-07-10 14:35:37 -0700

    Added problem URL

--- sortByHeight.py
+++ sortByHeight.py
@@ -1,3 +1,5 @@
+# https://app.codesignal.com/arcade/intro/level-3/D6qmdBL2NYz49XHwM
+
 def sortByHeight(a):
     trees_at = [i for i, v in enumerate(a) if v == -1]
     people = [i for i in a if i != -1]
