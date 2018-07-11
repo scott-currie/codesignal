a175798eb7edac3adf77bd67474ee90c0e9f9e7e
vitriolik <vitriolik@gmail.com>
2018-07-11 09:05:28 -0700

    Initial commit

--- like0hn0.py
+++ like0hn0.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python
 
 def main():
-    pass
+    grid = [[1,2,0],
+            [0,1,0],
+            [0,0,0]]
+    like0hn0(grid)
 
 def like0hn0(grid):
-    pass
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            n = grid[i][j]
+
+
 
 if __name__ == "__main__":
     main()
---
+++ solved/differentSymbolsNaive.py
@@ -0,0 +1,5 @@
+# https://app.codesignal.com/arcade/intro/level-8/8N7p3MqzGQg5vFJfZ
+
+def differentSymbolsNaive(s):
+    r = set(s)
+    return len(r)
